95b0d56f17f3dd6122563cd60d63ca42208be5d5
Denzel-Njuguna <denzelmakumi19@gmail.com>
2025-02-04 15:42:29 +0300

    correcting the the import issue

--- utils/productdb.js
+++ utils/productdb.js
@@ -17,10 +17,15 @@ const fetchproducts = (async(req,res)=>{
 const fetchproduct = (async (req,res) => {
     try {
         const{id} = req.entered_info
-        const query = ("select product_name,product_description,product_file,product_path from learning.product where product_id = $1")
+        const query = ("select product_name,product_description,product_file,product_path from learning.products where product_id = $1")
         const result = await pool.query(query,[id])
-        const product_data = result.rows[0]
-        return res.status(200).send(product_data)
+        if (result.rowCount != 0){
+            const product_data = result.rows[0]
+            return res.status(200).send(product_data)
+        }else{
+            console.log("sorry this product is out of stock")
+            return res.status(200).send({msg:`currently we are out of stock`})
+        }
     } catch (error) {
         console.log(`there is an error ${error} fetchproduct`)
     }
